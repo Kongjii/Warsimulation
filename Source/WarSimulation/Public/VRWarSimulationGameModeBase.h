722c849eed7325d98700e6d10224919271c97d04
GongBam <jkjy009@naver.com>
2024-05-01 15:34:34 +0900

    PlayerMove

---
+++ Source/WarSimulation/Public/VRWarSimulationGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VRWarSimulationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARSIMULATION_API AVRWarSimulationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/WarSimulation/WarSimulation.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "WarSimulation.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WarSimulation, "WarSimulation" );
