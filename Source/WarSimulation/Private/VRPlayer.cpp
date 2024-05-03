842e65fd824f8cd2fe4ec2506877ce1122bf2a21
GongBam <jkjy009@naver.com>
2024-05-03 20:30:26 +0900

    Teleport_Niagara

--- Source/WarSimulation/Private/VRPlayer.cpp
+++ Source/WarSimulation/Private/VRPlayer.cpp
@@ -4,19 +4,20 @@
 #include "VRPlayer.h"
 #include "Camera/CameraComponent.h"
 #include "MotionControllerComponent.h"
+#include <../../../../../../../Source/Runtime/Engine/Classes/Components/SkeletalMeshComponent.h>
 #include <../../../../../../../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputSubsystems.h>
 #include <../../../../../../../Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputComponent.h>
+#include <Components/CapsuleComponent.h>
+#include <../../../../../../../Plugins/FX/Niagara/Source/Niagara/Public/NiagaraComponent.h>
+#include <../../../../../../../Plugins/FX/Niagara/Source/Niagara/Classes/NiagaraDataInterfaceArrayFunctionLibrary.h>
 
-// Sets default values
 AVRPlayer::AVRPlayer()
 {
 	PrimaryActorTick.bCanEverTick = true;	
 
-	// VR카메라 컴포넌트를 생성하고 루트에 붙이고 싶다.
 	VRCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("VRCamera"));
 	VRCamera->SetupAttachment(RootComponent);
 
-	// 모션컨트롤러 왼손, 오른손 생성하고 루트에 붙이고 싶다.
 	MotionLeft = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("MotionLeft"));
 	MotionLeft->SetTrackingMotionSource(TEXT("Left"));
 	MotionLeft->SetupAttachment(RootComponent);
@@ -25,51 +26,44 @@ AVRPlayer::AVRPlayer()
 	MotionRight->SetTrackingMotionSource(TEXT("Right"));
 	MotionRight->SetupAttachment(RootComponent);
 
-	// 왼손, 오른손 스켈레탈메시컴포넌트를 만들고 모션 컨트롤러에 붙이고 싶다.
 	MeshLeft = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshLeft"));
 	MeshLeft->SetupAttachment(MotionLeft);
 
 	MeshRight = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshRight"));
 	MeshRight->SetupAttachment(MotionRight);
 
-	// 왼손 스켈레탈메시를 로드해서 적용하고 싶다.
 	ConstructorHelpers::FObjectFinder<USkeletalMesh> TempMeshLeft(TEXT("/Script/Engine.SkeletalMesh'/Game/Characters/MannequinsXR/Meshes/SKM_MannyXR_left.SKM_MannyXR_left'"));
-	// 로드 성공했다면 적용하고 싶다.
 	if (TempMeshLeft.Succeeded())
 	{
 		MeshLeft->SetSkeletalMesh(TempMeshLeft.Object);
 		MeshLeft->SetWorldLocationAndRotation(FVector(-2.981260f, -3.5f, 4.561753f), FRotator(-25, -180, 90));
 	}
 
-	// 오른손 스켈레탈메시를 로드해서 적용하고 싶다.
 	ConstructorHelpers::FObjectFinder<USkeletalMesh> TempMeshRight(TEXT("/Script/Engine.SkeletalMesh'/Game/Characters/MannequinsXR/Meshes/SKM_MannyXR_right.SKM_MannyXR_right'"));
-	// 로드 성공했다면 적용하고 싶다.
 	if (TempMeshRight.Succeeded())
 	{
 		MeshRight->SetSkeletalMesh(TempMeshRight.Object);
 		MeshRight->SetWorldLocationAndRotation(FVector(-2.981260f, 3.5f, 4.561753f), FRotator(25, 0, 90));
 	}
 
-	// 써클을 생성하고 충돌처리가 되지 않게 처리하고 싶다.
-	TeleportCircle = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TeleportCircle"));
-	TeleportCircle->SetupAttachment(RootComponent);
-	TeleportCircle->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	TeleportCircleVFX = CreateDefaultSubobject<UNiagaraComponent>(TEXT("TeleportCircleVFX"));
+	TeleportCircleVFX->SetupAttachment(RootComponent);
+	TeleportCircleVFX->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	TeleportTraceVFX = CreateDefaultSubobject<UNiagaraComponent>(TEXT("TeleportTraceVFX"));
+	TeleportCircleVFX->SetupAttachment(RootComponent);
 }
 
 void AVRPlayer::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	// 플레이어 컨트롤러를 가져오고 싶다.
-	//auto* pc = GetWorld()->GetFirstPlayerController();
 	auto* pc = Cast<APlayerController>(Controller);
-	// UEnhancedInputLocalPlayerSubsystem를 가져와서
 	if (pc)
 	{
 		auto subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(pc->GetLocalPlayer());
 		if (subsystem)
 		{
-			// AddMappingContext를 호출하고 싶다.
 			subsystem->AddMappingContext(IMC_VRPlayer, 0);
 		}
 	}
@@ -80,30 +74,19 @@ void AVRPlayer::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	// 만약 버튼이 눌러졌다면
 	if (true == bTeleporting)
 	{
-		FVector start = MeshRight->GetComponentLocation();
-		FVector end = start + MeshRight->GetRightVector() * 100000;
-
-		// 선 그리기
-		DrawLine(start, end);
-
-		// LineTrace를 해서 부딪힌 곳이 있다면
-		// HitTest(시작점, 끝점, &부딪힌 정보)
-		FHitResult hitInfo;
-		bool bHit = HitTest(start, end, hitInfo);
-		if (bHit)
+		if (bTeleportCurve)
 		{
-			// 그곳에 써클을 보이게 하고 배치하고 싶다.
-			TeleportCircle->SetWorldLocation(hitInfo.Location);
-			TeleportCircle->SetVisibility(true);
-		}
-		// 그렇지 않다면
-		else
-		{
-			// 써클을 보이지 않게 하고 싶다.
-			TeleportCircle->SetVisibility(false);
+			UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayVector(TeleportTraceVFX, FName("User.PointArray"), Points);
+			if (bTeleportCurve)
+			{
+				TickCurve();
+			}
+			else
+			{
+				TickLine();
+			}
 		}
 	}
 }
@@ -119,10 +102,7 @@ void AVRPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 		input->BindAction(IA_Move, ETriggerEvent::Triggered, this, &AVRPlayer::OnIAMove);
 		input->BindAction(IA_Turn, ETriggerEvent::Triggered, this, &AVRPlayer::OnIATurn);
 
-		// 텔레포트 입력을 등록하고 싶다.
-		// 눌렀을 때 ONIATeleportStart
 		input->BindAction(IA_Teleport, ETriggerEvent::Started, this, &AVRPlayer::ONIATeleportStart);
-		// 뗏을 때 ONIATeleportEnd
 		input->BindAction(IA_Teleport, ETriggerEvent::Completed, this, &AVRPlayer::ONIATeleportEnd);
 	}
 }
@@ -143,19 +123,29 @@ void AVRPlayer::OnIATurn(const FInputActionValue& value)
 
 void AVRPlayer::ONIATeleportStart(const FInputActionValue& value)
 {
-	// 누르면 써클이 보이고
 	bTeleporting = true;
+	TeleportTraceVFX->SetVisibility(true);
 }
 
 void AVRPlayer::ONIATeleportEnd(const FInputActionValue& value)
 {
-	// 떼면 안보이게 하고 싶다.
+	if (TeleportCircleVFX->GetVisibleFlag()) // 활성화 여부
+	{
+		if (bWarp)
+		{
+			DoWarp();
+		}
+		else
+		{
+			DoTeleport();
+		}
+	}
 	ResetTeleport();
 }
 
 void AVRPlayer::DrawLine(const FVector& start, const FVector& end)
 {
-	DrawDebugLine(GetWorld(), start, end, FColor::Red, false, -1, 0, 1);
+	//DrawDebugLine(GetWorld(), start, end, FColor::Red, false, -1, 0, 1);
 }
 
 bool AVRPlayer::HitTest(FVector start, FVector end, FHitResult& OuthitInfo)
@@ -167,8 +157,118 @@ bool AVRPlayer::HitTest(FVector start, FVector end, FHitResult& OuthitInfo)
 
 void AVRPlayer::ResetTeleport()
 {
-	// 써클 보이지 않게
-	// 텔레포트중이 아님
-	TeleportCircle->SetVisibility(false);
+	TeleportCircleVFX->SetVisibility(false);
 	bTeleporting = false;
+	TeleportTraceVFX->SetVisibility(false);
+}
+
+void AVRPlayer::DoTeleport()
+{
+	FVector height = FVector(0, 0, GetCapsuleComponent()->GetScaledCapsuleHalfHeight());
+	SetActorLocation(TeleportLocation + height);
+}
+
+void AVRPlayer::TickLine()
+{
+	FVector start = MeshRight->GetComponentLocation();
+	FVector end = start + MeshRight->GetRightVector() * 100000;
+
+	CheckHitTeleport(start, end);
+
+	DrawLine(start, end);
+
+	Points.Empty(2);
+	Points.Add(start);
+	Points.Add(end);
+}
+
+void AVRPlayer::TickCurve()
+{
+	MakeCurvePoints();
+
+	FHitResult hitInfo;
+	int maxPoints = Points.Num();
+	for (int i = 0; i < Points.Num() - 1; i++)
+	{
+		if (CheckHitTeleport(Points[i], Points[i + 1]))
+		{
+			maxPoints = i + 1;
+			break;
+		}
+	}
+
+	Points.SetNum(maxPoints);
+
+	DrawCurve(maxPoints);
+}
+
+bool AVRPlayer::CheckHitTeleport(const FVector& start, FVector& end)
+{
+	FHitResult hitInfo;
+	bool bHit = HitTest(start, end, hitInfo);
+	if (bHit && hitInfo.GetActor()->GetActorNameOrLabel().Contains(TEXT("Floor")))
+	{
+		end = hitInfo.ImpactPoint;
+		TeleportLocation = hitInfo.Location; 
+		TeleportCircleVFX->SetWorldLocation(hitInfo.Location);
+		TeleportCircleVFX->SetVisibility(true);
+	}
+	else
+	{
+		TeleportCircleVFX->SetVisibility(false);
+	}
+	return bHit;
+}
+
+void AVRPlayer::MakeCurvePoints()
+{
+	Points.Empty(CurveStep);
+	FVector gravity(0, 0, -981);
+	float simDT = 1.f / 60.f;
+	FVector Point = MeshRight->GetComponentLocation();
+	FVector velocity = MeshRight->GetRightVector() * 1000;
+	Points.Add(Point);
+
+	for (int i = 0; i < CurveStep; i++)
+	{
+		Point += velocity * simDT + 0.5f * gravity * simDT * simDT;
+		velocity += gravity * simDT;
+		Points.Add(Point);
+	}
+}
+
+void AVRPlayer::DrawCurve(int max)
+{
+	for (int i = 0; i < max - 1; i++)
+	{
+		DrawLine(Points[i], Points[i + 1]);
+	}
+}
+
+void AVRPlayer::DoWarp()
+{
+	if ( false == bWarp )
+		return;
+
+	CurrentTime = 0;
+	
+	FVector height = FVector(0, 0, GetCapsuleComponent()->GetScaledCapsuleHalfHeight());
+	FVector tarLoc = TeleportLocation + height;
+	FVector originLoc = GetActorLocation();
+
+	GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	GetWorld()->GetTimerManager().SetTimer(WarpTimerHandle, [&, originLoc, tarLoc]() {
+			CurrentTime += GetWorld()->GetDeltaSeconds();
+
+			float alpha = CurrentTime / WarpTime;
+			FVector curLoc = FMath::Lerp(curLoc, tarLoc, alpha);
+			SetActorLocation(curLoc);
+
+			if (alpha >= 1) {
+				GetWorld()->GetTimerManager().ClearTimer(WarpTimerHandle);
+				SetActorLocation(tarLoc);
+				GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+			}
+	}, 0.033333f, true);
 }
--- Source/WarSimulation/Public/VRPlayer.h
+++ Source/WarSimulation/Public/VRPlayer.h
@@ -15,11 +15,9 @@ class WARSIMULATION_API AVRPlayer : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
 	AVRPlayer();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
@@ -27,57 +25,78 @@ public:
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	// VR카메라 컴포넌트를 생성하고 루트에 붙이고 싶다.
 	UPROPERTY(EditDefaultsOnly)
 	class UCameraComponent* VRCamera;
 
-	// 모션컨트롤러 왼손, 오른손 생성하고 루트에 붙이고 싶다.
 	UPROPERTY(EditDefaultsOnly)
 	class UMotionControllerComponent* MotionLeft;
 
 	UPROPERTY(EditDefaultsOnly)
 	class UMotionControllerComponent* MotionRight;
 
-	// 왼손, 오른손 스켈레탈메시컴포넌트를 만들고 모션 컨트롤러에 붙이고 싶다.
 	UPROPERTY(EditDefaultsOnly)
 	class USkeletalMeshComponent* MeshLeft;
 
 	UPROPERTY(EditDefaultsOnly)
 	class USkeletalMeshComponent* MeshRight;
 
-	// 왼손, 오른손 스켈레탈메시를 로드해서 적용하고 싶다.
 	UPROPERTY(EditDefaultsOnly, Category = VR)
 	class UInputMappingContext* IMC_VRPlayer;
 
 	UPROPERTY(EditDefaultsOnly, Category = VR)
 	class UInputAction* IA_Move;
 
-	// Move 생성
 	void OnIAMove(const FInputActionValue& value);
 
 	UPROPERTY(EditDefaultsOnly, Category = VR)
 	class UInputAction* IA_Turn;
 
-	// 마우스 좌우 생성
 	void OnIATurn(const FInputActionValue& value);
 
-	// 텔레포트 처리를 위해서 써클을 표현하고 싶다.
 	UPROPERTY(EditDefaultsOnly, Category = VR)
-	class UStaticMeshComponent* TeleportCircle;
+	class UNiagaraComponent* TeleportCircleVFX;
 
-	// 텔레포트중인가? 여부를 기억하고 싶다.
 	bool bTeleporting;
 
-	// 입력처리와 그에 해당하는 함수를 구현하고 싶다.
 	UPROPERTY(EditDefaultsOnly, Category = VR)
 	class UInputAction* IA_Teleport;
 
-	// Teleport 생성
 	void ONIATeleportStart(const FInputActionValue& value);
 	void ONIATeleportEnd(const FInputActionValue& value);
 
 	void DrawLine(const FVector& start, const FVector& end);
-private:
+
 	bool HitTest(FVector start, FVector end, FHitResult& OuthitInfo);
 	void ResetTeleport();
+
+	FVector TeleportLocation;
+	void DoTeleport();
+
+	UPROPERTY(EditDefaultsOnly, Category = VR)
+	bool bTeleportCurve = true;
+
+	void TickLine();
+	void TickCurve();
+
+	bool CheckHitTeleport(const FVector& start, FVector& end);
+
+	UPROPERTY(EditDefaultsOnly, Category = VR)
+	int32 CurveStep = 200;
+
+	TArray<FVector> Points;
+	void MakeCurvePoints();
+
+	void DrawCurve(int max);
+
+	UPROPERTY(EditDefaultsOnly, Category = VR)
+	class UNiagaraComponent* TeleportTraceVFX;
+
+	UPROPERTY(EditDefaultsOnly, Category = VR)
+	bool bWarp = true;
+
+	FTimerHandle WarpTimerHandle;
+	float CurrentTime;
+	float WarpTime = 0.2f;
+
+	void DoWarp();
 };
